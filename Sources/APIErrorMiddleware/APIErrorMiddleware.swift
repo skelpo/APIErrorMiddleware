47c01500af2e7c395647ba1b5f324dac9e8ea045
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-20 12:25:12 -0500

    Created APIErrorMiddleware class

--- Sources/APIErrorMiddleware/APIErrorMiddleware.swift
+++ Sources/APIErrorMiddleware/APIErrorMiddleware.swift
@@ -1,1 +1,45 @@
+import Vapor
+import Foundation
 
+public final class APIErrorMiddleware: Middleware {
+    public init() {}
+    
+    public func respond(to request: Request, chainingTo next: Responder) throws -> Future<Response> {
+        let result = request.eventLoop.newPromise(Response.self)
+        
+        do {
+            return try next.respond(to: request).do({ (response) in
+                result.succeed(result: response)
+            }).catch({ (error) in
+                result.succeed(result: self.response(for: error, with: request))
+            })
+        } catch {
+            result.succeed(result: self.response(for: error, with: request))
+        }
+        
+        return result.futureResult
+    }
+    
+    private func response(for error: Error, with request: Request) -> Response {
+        let message: String
+        let status: HTTPStatus?
+        
+        if let error = error as? AbortError {
+            message = error.reason
+            status = error.status
+        } else {
+            let error = error as CustomStringConvertible
+            message = error.description
+            status = nil
+        }
+        
+        let json = (try? JSONEncoder().encode(["error": message])) ?? message.data(using: .utf8) ?? Data()
+        let httpResponse = HTTPResponse(
+            status: status ?? .badRequest,
+            headers: ["Content-Type": "application/json"],
+            body: HTTPBody(data: json)
+        )
+        
+        return Response(http: httpResponse, using: request.sharedContainer)
+    }
+}
