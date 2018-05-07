be9c50c1bfc9afd587d193ad3e5fb274bcbabd1c
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-07 14:24:56 -0500

    Updated Vapor to full 3.0 release

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "APIErrorMiddleware", targets: ["APIErrorMiddleware"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc")
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0")
     ],
     targets: [
         .target(name: "APIErrorMiddleware", dependencies: ["Vapor"]),
